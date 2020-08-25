c74cbbe39be58bdd15949210c6a316e1bcf36eec
Ubuntu <ubuntu@ip-172-31-57-225.us-west-2.compute.internal>
2020-08-25 19:25:42 +0000

    update

--- models/x86/compile.py
+++ models/x86/compile.py
@@ -94,8 +94,8 @@ def compile_via_tvm(sym, arg_params, aux_params, symbol_file, data_shape):
 
     print('Model Load!')
     import tensorizer
-    with tvm.transform.PassContext(config={'tir.add_lower_pass': [(1, tensorizer.rewrite)]},
-                                   trace=tracer, opt_level=4):
+    with tvm.transform.PassContext(disabled_pass={'FuseOps'}, config={'tir.add_lower_pass': [(1, tensorizer.rewrite)]},
+                                   trace=tracer, opt_level=3):
             graph, lib, params = relay.build_module.build(
                 mod, target=target, params=params)
             base = './compiled/' + symbol_file.split('/')[-1].replace('.json','')
