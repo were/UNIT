e1f7400a90eca4197feb9030dda6ab59f4d3a598
Jian Weng <jian.weng@ucla.edu>
2020-08-27 04:47:05 +0000

    stash commit

--- apps/gpu/relay.py
+++ apps/gpu/relay.py
@@ -13,7 +13,7 @@ from tvm.relay import op
 #t0, t1 = eval(input())
 #n, c, h, w = map(int, t0)
 #oc, ic, kh, kw = map(int, t1)
-n, c, h, w = 1, 192, 18, 18
+n, c, h, w = 1, 192, 16, 16
 oc, ic, kh, kw = 192, c, 3, 3
 
 var_x = relay.var('x', shape=(n, c, h, w), dtype='float32')
@@ -38,25 +38,25 @@ def tracer(module, info, is_before):
     #else:
     #    print('Executes: ', info.name, (time.time() - timing) * 1000)
 
-passes = [(1, tensorizer.rewrite)]
-with tvm.transform.PassContext(opt_level=4, trace=tracer, config={'tir.add_lower_pass': passes}):
-#with tvm.transform.PassContext(opt_level=4, trace=tracer):
-    #graph, lib, params = tvm.relay.build(module, target='cuda -libs=cublas,cudnn')
-    graph, lib, params = tvm.relay.build(module, target='nvptx -libs=cublas,cudnn')
-    #from tvm.contrib import graph_runtime as runtime
-    from tvm.contrib.debugger import debug_runtime as runtime
-    module = runtime.create(graph, lib, tvm.gpu())
+for i in ['fuse', 'pad']:
+    for j in [16, 32, 64]:
+        from tensorizer import tune
+        tune.padding = i
+        tune.splitk = j
+        passes = [(1, tensorizer.rewrite)]
+        with tvm.transform.PassContext(opt_level=3, trace=tracer, config={'tir.add_lower_pass': passes}):
+        #with tvm.transform.PassContext(opt_level=4, trace=tracer):
+            #graph, lib, params = tvm.relay.build(module, target='cuda -libs=cublas,cudnn')
+            graph, lib, params = tvm.relay.build(module, target='nvptx -libs=cublas,cudnn')
+            print('compiled')
+            from tvm.contrib import graph_runtime as runtime
+            #from tvm.contrib.debugger import debug_runtime as runtime
+            func = runtime.create(graph, lib, tvm.gpu())
 
-    x_ =(np.random.randn(n, c, h, w) * 128).astype('float32')
-    module.set_input('x', x_)
-    module.run()
-    module.run()
-    module.run()
-    module.run()
-    module.run()
+            x_ =(np.random.randn(n, c, h, w) * 128).astype('float32')
+            func.set_input('x', x_)
+            timer = func.module.time_evaluator('run', ctx=tvm.gpu(), number=1, repeat=10)
+            timed = timer()
 
-    #timer = module.module.time_evaluator('run', ctx=tvm.gpu(), number=1, repeat=1)
-    #timed = timer()
-
-    #print((n * oc * (h - kh + 1) * (w - kw + 1)) * (kh * kw * ic) / timed.mean / 1e9)
-    #print('%d us' % int(timed.mean * 1e6))
+            print((n * oc * (h - kh + 1) * (w - kw + 1)) * (kh * kw * ic) / timed.mean / 1e9)
+            print('%d us' % int(timed.mean * 1e6))
--- python/tensorizer/tune.py
+++ python/tensorizer/tune.py
@@ -1,7 +1,8 @@
 cpu_idx = None
 total_idx = None
-padding = None
-splitk = None
 ashape = None
 bshape = None
 strides = None
+
+padding = None
+splitk = None
