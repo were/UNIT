45fa14f2bad94b977ecc3fd48a01a1430303a8f6
Jian Weng <jian.weng@ucla.edu>
2020-11-17 04:22:13 +0000

    debug runtime

--- apps/gpu/relay.py
+++ apps/gpu/relay.py
@@ -5,11 +5,16 @@ import sys
 import numpy as np
 from tvm import relay
 from tvm import autotvm
+import os
+import argparse
 
 import topi
 from tvm.relay import op
 
-
+ap = argparse.ArgumentParser()
+ap.add_argument('--target', type=str, default='nvptx')
+args = ap.parse_args()
+target =args.target + ' -libs=cublas,cudnn'
 #t0, t1 = eval(input())
 #n, c, h, w = map(int, t0)
 #oc, ic, kh, kw = map(int, t1)
@@ -44,10 +49,9 @@ tune.enable = False
 
 def run():
     passes = [(1, tensorizer.rewrite)]
-    with tvm.transform.PassContext(opt_level=3, trace=tracer, config={'tir.add_lower_pass': passes}):
-    #with tvm.transform.PassContext(opt_level=4, trace=tracer):
-        #graph, lib, params = tvm.relay.build(module, target='cuda -libs=cublas,cudnn')
-        graph, lib, params = tvm.relay.build(module, target='nvptx -libs=cublas,cudnn')
+    config = {'tir.add_lower_pass': passes} if target.startswith('nvptx') else {}
+    with tvm.transform.PassContext(opt_level=3, trace=tracer, config=config):
+        graph, lib, params = tvm.relay.build(module, target=target)
         #from tvm.contrib import graph_runtime as runtime
         from tvm.contrib.debugger import debug_runtime as runtime
         func = runtime.create(graph, lib, tvm.gpu())
@@ -55,21 +59,28 @@ def run():
 
         x_ =(np.random.randn(n, c, h, w) * 128).astype('float32')
         func.set_input('x', x_)
-        #timer = func.module.time_evaluator('run', ctx=tvm.gpu(), number=1, repeat=10)
-        timed = []
-        for i in range(10):
-            func.run()
-            for node, time in zip(func.debug_datum._nodes_list, func.debug_datum._time_list):
-                if 'conv2d' in node['name']:
-                    timed.append(time[0])
-        #timed = timer()
-        #while np.var(timed.results) > 1e-5:
-        #    timed = timer()
-        return np.mean(timed)
+        timer = func.module.time_evaluator('run', ctx=tvm.gpu(), number=1, repeat=10)
+        #timed = []
+        #for i in range(10):
+        #    func.run()
+        #    for node, time in zip(func.debug_datum._nodes_list, func.debug_datum._time_list):
+        #        if 'conv2d' in node['name']:
+        #            timed.append(time[0])
+        timed = timer()
+        while np.var(timed.results) > 1e-5:
+            timed = timer()
+        return timed.mean
+        #return np.mean(timed)
 
 base = None
 timed = run()
 base = timed * 1e6
+
+if target.startswith('cuda'):
+    with open(os.getenv('HOME') + '/tune.csv', 'a') as f:
+        f.write(','.join(map(str, [n, c, h, w, oc, ic, kh, kw, sh, sw])) + ',' + str(timed * 1e6) + '\n')
+    quit()
+
 relay.backend.compile_engine.get().clear()
 
 results = []
@@ -84,6 +95,7 @@ for i in [None, 'fuse', 'pad'] if ow < 32 else [None]:
 
         relay.backend.compile_engine.get().clear()
         j <<= 1
+        print(tune.total_idx)
         if j > tune.total_idx:
             break
 
--- apps/gpu/tune.py
+++ apps/gpu/tune.py
@@ -1,4 +1,9 @@
 import subprocess
+import argparse
+
+ap = argparse.ArgumentParser()
+ap.add_argument('--target', type=str, default='nvptx')
+args = ap.parse_args()
 
 #with open('/home/ubuntu/shapes.raw', 'r') as f:
 #    shapes = []
@@ -14,8 +19,10 @@ import subprocess
 
 shapes = [(1, 288, 35, 35, 384, 288, 3, 3, 2, 2), (1, 160, 9, 9, 224, 160, 3, 3, 1, 1), (1, 1056, 7, 7, 192, 1056, 1, 1, 1, 1), (1, 80, 73, 73, 192, 80, 3, 3, 1, 1), (1, 128, 16, 16, 128, 128, 3, 3, 1, 1), (1, 192, 16, 16, 192, 192, 3, 3, 1, 1), (1, 256, 16, 16, 256, 256, 3, 3, 1, 1), (1, 1024, 14, 14, 512, 1024, 1, 1, 1, 1), (1, 128, 16, 16, 160, 128, 3, 3, 1, 1), (1, 576, 14, 14, 192, 576, 1, 1, 1, 1), (1, 96, 16, 16, 128, 96, 3, 3, 1, 1), (1, 1024, 14, 14, 256, 1024, 1, 1, 1, 1), (1, 576, 14, 14, 128, 576, 1, 1, 1, 1), (1, 64, 29, 29, 96, 64, 3, 3, 1, 1), (1, 64, 56, 56, 128, 64, 1, 1, 2, 2), (1, 608, 14, 14, 192, 608, 1, 1, 1, 1)]
 
+target = args.target
+
 for i in shapes:
     with open('input', 'w') as f:
         f.write(' '.join(map(str, i)))
     print('tuning:', i)
-    subprocess.check_output('python relay.py < input', shell=True)
+    subprocess.check_output(f'python relay.py --target={target} < input', shell=True)
